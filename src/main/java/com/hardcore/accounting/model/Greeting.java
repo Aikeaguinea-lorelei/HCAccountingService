97f5dd5442efb540bc3f4fd25bdfd64b194298aa
Aikeaguinea-Lorelei <2656611469@qq.com>
2023-06-07 15:43:23 +0800

    feat(controllers):implement the hello controller for first api

---
+++ src/main/java/com/hardcore/accounting/model/Greeting.java
@@ -0,0 +1,21 @@
+package com.hardcore.accounting.model;
+
+public class Greeting {
+    private final Long id;
+    private String name;
+
+//    生成Getter和setter的快捷键: alt + insert
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Greeting(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
